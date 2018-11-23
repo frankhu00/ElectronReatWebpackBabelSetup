e2e9087823ff1eae7ee81f306d58e16a25cbacfa
Frank Hu <=>
2018-11-23 01:10:48 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config = {
+    target: 'electron-renderer',
+    entry: ['@babel/polyfill', './app/src/app.js'],
+    output: {
+        publicPath: 'dist/',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'bundle.css',
+            disable: false,
+            allChunks: true
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
+
+module.exports = config;
